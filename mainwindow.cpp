17bb03fe04f7f00a592fc6e44b058e35b4dd3ce0
ghosty <gicka39@outlook.com>
2024-12-04 12:27:52 +0800

    Add database connection and main UI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,3 +13,28 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pushButton_login_clicked(){
+    qDebug() << "尝试登录";
+
+}
+
+void MainWindow::on_checkBox_toggled(bool checked)
+{
+    if(checked)
+    {
+        ui->lineEdit_username->setEchoMode(QLineEdit::Normal);
+    }
+    else
+    {
+        ui->lineEdit_password->setEchoMode(QLineEdit::Password);
+    }
+}
+
+void MainWindow::on_pushButton_register_clicked(){
+
+}
+
+void MainWindow::on_pushButton_exit_clicked(){
+    QApplication::quit();
+}
